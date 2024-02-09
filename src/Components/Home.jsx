d3716e2547c0cf9e414733efc129885d7a1e6fe6
Srikar2273 <kbnsrikar2273@gmail.com>
2024-02-09 17:17:30 +0530

    Navbar and Home Page positions and change animation color

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,12 @@
 import { Box, Typography } from "@mui/material";
 import React from 'react'
-import Firstpage from "./firstpage";
 const Home = () => {
     return (
         <>
             <Box>
-                <Firstpage />
+                <Typography color='white'>
+                    Hi I'm Srikar
+                </Typography>
             </Box>
         </>
     )
